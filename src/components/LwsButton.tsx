4c4872f1619c997ebe23a6fb129760cb88eab50d
KangDroid <hyunwoo286@gmail.com>
2022-03-31 23:34:12 +0900

    LWSButton: Use Styled/Functional Component

--- src/components/LwsButton.tsx
+++ src/components/LwsButton.tsx
@@ -1,26 +1,42 @@
-import { Component } from 'react';
+import { Component, ReactElement, CSSProperties } from 'react';
+import styled from 'styled-components';
 import './LwsButton.css';
 
-type LwsButtonProps = {
-  title: string;
-  width?: string | undefined;
-  type?: 'button' | 'submit' | 'reset' | undefined;
-};
-
-class LwsButton extends Component<LwsButtonProps> {
-  render() {
-    return (
-      <button
-        className="LwsButton"
-        style={{
-          width: this.props.width,
-        }}
-        type={this.props.type}
-      >
-        {this.props.title}
-      </button>
-    );
-  }
+interface LwsButtonOverrideableStyle {
+  width?: string;
+  height?: string;
 }
 
-export default LwsButton;
+interface LwsButtonProps extends LwsButtonOverrideableStyle {
+  title: string;
+  type?: 'button' | 'submit' | 'reset' | undefined;
+}
+
+export const LwsStyledButtonBase = styled.button<LwsButtonOverrideableStyle>`
+  &:active {
+    background: #b4b4fa;
+  }
+  background: #c8c8fa;
+  font-family: Ubuntu Mono;
+  padding: 7px 10px;
+  height: ${(prop) => prop.height ?? '30px'};
+  width: ${(prop) => prop.width ?? '70px'};
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 16px;
+`;
+
+export function LwsButton({
+  title,
+  width,
+  height,
+  type,
+}: LwsButtonProps): ReactElement {
+  return (
+    <LwsStyledButtonBase width={width} height={height} type={type}>
+      {title}
+    </LwsStyledButtonBase>
+  );
+}
